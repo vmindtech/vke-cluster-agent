9e8b7e29850083cb9059027bb96c583d7275bd04
Mert Öngengil <mertongngl@gmail.com>
2025-02-28 13:54:24 +0300

    Add custom HTTP client settings for OpenStack authentication

--- internal/service/openstack.go
+++ internal/service/openstack.go
@@ -3,6 +3,7 @@ package service
 import (
 	"crypto/tls"
 	"net/http"
+	"time"
 
 	"github.com/gophercloud/gophercloud"
 	"github.com/gophercloud/gophercloud/openstack"
@@ -21,6 +22,14 @@ func NewOpenstackService() IOpenstackService {
 }
 
 func (o *openstackService) ValidateAndCreateSession(pjID, applicationCredentialID, applicationCredentialSecret, identityURL string) (*gophercloud.ProviderClient, error) {
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{
+			InsecureSkipVerify: true,
+		},
+		TLSHandshakeTimeout: 30 * time.Second,
+		IdleConnTimeout:     90 * time.Second,
+	}
+
 	authOpts := gophercloud.AuthOptions{
 		IdentityEndpoint:            identityURL,
 		ApplicationCredentialID:     applicationCredentialID,
@@ -28,18 +37,24 @@ func (o *openstackService) ValidateAndCreateSession(pjID, applicationCredentialI
 		AllowReauth:                 true,
 	}
 
-	provider, err := openstack.AuthenticatedClient(authOpts)
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   time.Second * 60,
+	}
+
+	providerClient, err := openstack.NewClient(identityURL)
+	if err != nil {
+		klog.Errorf("Failed to create OpenStack client - identityURL: %s, error: %v", identityURL, err)
+		return nil, err
+	}
+	providerClient.HTTPClient = *client
+
+	err = openstack.Authenticate(providerClient, authOpts)
 	if err != nil {
 		klog.Errorf("OpenStack authentication failed - identityURL: %s, projectID: %s, applicationCredentialID: %s, error: %v",
 			identityURL, pjID, applicationCredentialID, err)
 		return nil, err
 	}
 
-	provider.HTTPClient.Transport = &http.Transport{
-		TLSClientConfig: &tls.Config{
-			InsecureSkipVerify: true,
-		},
-	}
-
-	return provider, nil
+	return providerClient, nil
 }
